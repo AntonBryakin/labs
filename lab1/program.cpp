e576308fba6dd49e51ac69dd06f4755260f44eae
AntonBryakin <129679541+AntonBryakin@users.noreply.github.com>
2024-09-13 14:38:08 +0300

    task4

--- lab1/program.cpp
+++ lab1/program.cpp
@@ -4,14 +4,26 @@
 int main() {
     srand(time(NULL));
 
-    int max_a, min_a;
+    int max_a, min_a, size;
+    printf("Введите размер массива: ");
+    scanf("%d", &size);
     int summ = 0;
-    int a[5][5];
+    int *arr_summ;
+    arr_summ = (int*)malloc(size * sizeof(int));
+    int **a;
+    a = (int**)malloc(size * sizeof(int*));
+
+    for (int i = 0; i < size; i++)
+    {
+        a[i] = (int*)malloc(size * sizeof(int));
+    }
+
+   
 
     printf("\nМассив: \n");
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < size; i++)
     {   
-        for (int j = 0; j < 5; j++)
+        for (int j = 0; j < size; j++)
         {
             a[i][j] = rand()%100;
             printf("%d ", a[i][j]);
@@ -19,15 +31,23 @@ int main() {
         printf("\n");
     };
     printf("\n");
-    for (int i = 0; i < 5; i++)
+    
+    for (int i = 0; i < size; i++)
     {   
-        for (int j = 0; j < 5; j++)
+        for (int j = 0; j < size; j++)
         {
             summ = summ + a[i][j];
         }
+        arr_summ[i] = summ;
         printf("Сумма %d строки: %d\n", i+1, summ);
         summ = 0;
     };
+
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ",arr_summ[i]);
+    }
+    
 
     return 0;
 }
