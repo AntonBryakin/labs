0d522f6357bcd830adebd7fdae007ab8a13b7068
AntonBryakin <129679541+AntonBryakin@users.noreply.github.com>
2024-09-13 08:53:35 +0300

    task3

--- lab1/program.cpp
+++ lab1/program.cpp
@@ -4,15 +4,20 @@
 int main() {
     srand(time(NULL));
 
+    int size;
+    printf("Введите размер массива: ");
+    scanf("%d", &size);
     int max_a, min_a;
-    int a[10]; 
+    int *a;
+    a = (int*)malloc(size * sizeof(int));
 
-    min_a = max_a = a[0];
 
-    printf("Массив: ");
-    for (int i = 1; i <= 9; i++)
-    {
+    printf("\nМассив: ");
+    for (int i = 0; i < size; i++)
+    {   
         a[i] = rand()%100;
+        if (i == 0) min_a = max_a = a[i];
+        
         if (a[i] > max_a) {
             max_a = a[i];
         };
@@ -21,8 +26,8 @@ int main() {
         };
 
         printf("%d ",a[i]);
-    }
-    
+    };
+    free(a);
     printf("\nМаксимальное число в массиве: %d\nМинимальное число в массиве: %d", max_a,min_a);
 
     return 0;
