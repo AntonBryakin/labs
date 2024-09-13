2113b526f04947d38bb8e36a9b96a68bc034ee70
AntonBryakin <129679541+AntonBryakin@users.noreply.github.com>
2024-09-13 16:52:31 +0300

    task1.2

--- lab2/programm.cpp
+++ lab2/programm.cpp
@@ -1,1 +1,67 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+int main(void)
+{
+	setvbuf(stdin, NULL, _IONBF, 0);
+	setvbuf(stdout, NULL, _IONBF, 0);
+
+	clock_t start, end; // объявляем переменные для определения времени выполнения
+
+	int i=0, j=0, r, size;
+
+    for (size = 100; size <= 10000; size = size+100)
+    {
+	    int **a, **b, **c, elem_c;
+        
+        a = (int**)malloc(size * sizeof(int*));
+        b = (int**)malloc(size * sizeof(int*));
+        c = (int**)malloc(size * sizeof(int*));
+
+        for (int i = 0; i < size; i++)
+        {
+            a[i] = (int*)malloc(size * sizeof(int));
+            b[i] = (int*)malloc(size * sizeof(int));
+            c[i] = (int*)malloc(size * sizeof(int));
+        }
+
+        int starttime = clock();
+        srand(time(NULL)); // инициализируем параметры генератора случайных чисел
+        while(i<size)
+        {
+            while(j<size)
+            {
+                a[i][j]=rand()% 100 + 1; // заполняем массив случайными числами
+                b[i][j]=rand()% 100 + 1; // заполняем массив случайными числами
+                j++;
+            }
+            i++;
+        }
+        srand(time(NULL)); // инициализируем параметры генератора случайных чисел
+        i=0; j=0;
+
+        for(i=0;i<size;i++)
+        {
+            for(j=0;j<size;j++)
+            {
+                elem_c=0;
+                for(r=0;r<size;r++)
+                {
+                    elem_c=elem_c+a[i][r]*b[r][j];
+                    c[i][j]=elem_c;
+                }
+            }
+        }
+
+        float diff = ((clock()) - starttime)/float(CLOCKS_PER_SEC);
+        printf("При размере матрицы %d на %d, время выполнения: %f\n", size, size, diff);
+        free(a);
+        free(b);
+        free(c);
+    }
+
+
+	return(0);
+}
