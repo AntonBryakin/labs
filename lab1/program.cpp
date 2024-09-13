19914a775286016d3cc9e341cf1ae7148ceeeaad
AntonBryakin <129679541+AntonBryakin@users.noreply.github.com>
2024-09-13 15:45:55 +0300

    task5

--- lab1/program.cpp
+++ lab1/program.cpp
@@ -1,53 +1,57 @@
 #include <iostream>
 #include <stdlib.h>
+#include<string.h>
 
-int main() {
-    srand(time(NULL));
+int   main(void)
+{
+	setvbuf(stdin, NULL, _IONBF, 0);
+	setvbuf(stdout, NULL, _IONBF, 0);
+	int i;
+    bool search = false;
+	struct student
+	{
+		char famil[20];
+		char name[20], facult[20];
+		int Nomzach;
+	} stud[4];
 
-    int max_a, min_a, size;
-    printf("Введите размер массива: ");
-    scanf("%d", &size);
-    int summ = 0;
-    int *arr_summ;
-    arr_summ = (int*)malloc(size * sizeof(int));
-    int **a;
-    a = (int**)malloc(size * sizeof(int*));
+	for(i=0;i<3;i++)
+	{
+		printf("Введите фамилию студента\n"); scanf ("%20s",stud[i].famil);
+	}
+	for(i=0;i<3;i++)
+	{
+		printf("Введите имя студента %s\n",stud[i].famil); scanf ("%20s",stud[i].name);
+	}
+	for(i=0;i<3;i++)
+	{
+		printf("Введите название факультета студента %s %s\n",stud[i].famil,stud[i].name); scanf ("%20s",stud[i].facult);
+	}
+	for(i=0;i<3;i++)
+	{
+		printf("Введите номер зачётной книжки студента %s %s\n",stud[i].famil,stud[i].name); scanf ("%d",&stud[i].Nomzach);
+	}
 
-    for (int i = 0; i < size; i++)
+	for(i=0;i<3;i++)
+		{
+			printf("Cтудент %s %s обучается на факультете %s, номер зачётной книжки %d \n",stud[i].famil,stud[i].name,
+					stud[i].facult,stud[i].Nomzach);
+		}
+    
+    printf("Введите фамилию искомого студента\n"); scanf("%20s",stud[4].famil);
+    printf("Введите имя искомого студента %s\n",stud[4].famil); scanf("%20s",stud[4].name);
+    
+    for (int i = 0; i < 3; i++)
     {
-        a[i] = (int*)malloc(size * sizeof(int));
+        if ((!strcmp(stud[i].famil,stud[4].famil)) and (!strcmp(stud[i].name,stud[4].name)))
+        {
+            printf("Студент %s %s найден!", stud[i].famil, stud[i].name);
+            search = true;
+        }
+    }
+    
+    if (search == false) {
+        printf("Такой студент не найден!");
     }
 
-   
-
-    printf("\nМассив: \n");
-    for (int i = 0; i < size; i++)
-    {   
-        for (int j = 0; j < size; j++)
-        {
-            a[i][j] = rand()%100;
-            printf("%d ", a[i][j]);
-        }
-        printf("\n");
-    };
-    printf("\n");
-    
-    for (int i = 0; i < size; i++)
-    {   
-        for (int j = 0; j < size; j++)
-        {
-            summ = summ + a[i][j];
-        }
-        arr_summ[i] = summ;
-        printf("Сумма %d строки: %d\n", i+1, summ);
-        summ = 0;
-    };
-
-    for (int i = 0; i < size; i++)
-    {
-        printf("%d ",arr_summ[i]);
-    }
-    
-
-    return 0;
 }
