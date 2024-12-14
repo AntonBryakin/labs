180afa287961d68b9b977048a13b764fe61aec46
AntonBryakin <129679541+AntonBryakin@users.noreply.github.com>
2024-12-14 10:54:01 +0300

    someThing

--- lab5/programm.cpp
+++ lab5/programm.cpp
@@ -8,13 +8,13 @@ int factorial (int n){
 
 int main() {
     srand(time(NULL));
-    int n, **G=NULL, sizeGraf = 0, countIzol = 0, countDom = 0, countEnd = 0;
+    int n, m=0, GI=NULL, **G=NULL, sizeGraf = 0;
 
     printf("Введите количество вершин графа:");
     scanf("%d",&n);
 
 
-    G = (int **)malloc(n*sizeof(int*));
+    G = (int )malloc(n*sizeof(int*));
 
     for (int i=0; i<n; i++)
         G[i] = (int*)malloc(n*sizeof(int));
@@ -43,17 +43,71 @@ int main() {
         }
         printf("\n");
     }
+
     printf("Размер графа равен %d \n", sizeGraf);
 
     for (int i=0; i<n; i++){
         int sizeStr = 0;
-        countDom = 0;
-        countIzol = 0;
-        countEnd = 0;
         for (int j = 0; j<n; j++){
-            if ((G[i][j] == 1) and (i != j)) sizeStr = sizeStr + 1;
+            if ((G[i][j] == 1) and (i!=j)) sizeStr++;
         }
         if (sizeStr == 0) printf("Вершина %d изолированная\n", i+1);
+        if (sizeStr == 1) printf("Вершина %d концевая\n", i+1);
+        if (sizeStr >= n-1) printf("Вершина %d доминирующая\n", i+1);
+    }
+
+    m = sizeGraf;
+    GI = (int **)malloc(m*sizeof(int*));
+    for (int i=0; i<m; i++)
+        GI[i] = (int*)malloc(n*sizeof(int));
+
+    for (int i=0; i<n; i++){
+        for (int j=0; j<m; j++){
+            GI[j][i] = 0;
+        }
+    }
+
+    int num = 0;
+    for (int i=0; i<n; i++){
+        for (int j=i; j<n; j++){
+            if (G[i][j]==1) {
+                if (i==j)
+                    GI[num][i] = 2;
+                else {
+                    GI[num][i] = 1;
+                    GI[num][j] = 1;
+                }
+                num++;
+            }
+        }
+    }
+
+    sizeGraf = 0;
+    for (int i=0; i<n; i++){
+        for (int j=0; j<m; j++){
+            if (GI[j][i]>0) sizeGraf = sizeGraf + GI[j][i]; 
+        }
+    }
+    sizeGraf = sizeGraf/2;
+
+    printf("\n");
+
+    for (int i=0; i<n; i++){
+        for (int j=0; j<m; j++){
+            printf("%d ",GI[j][i]);
+        }
+        printf("\n");
+    }
+    printf("Размер графа равен %d \n", sizeGraf);
+
+    for (int i=0; i<n; i++){
+        int sizeStr = 0;
+        bool petlya = false;
+        for (int j = 0; j<m; j++){
+            if (GI[j][i] == 2) petlya == true;
+            if (GI[j][i] == 1) sizeStr = sizeStr + 1;
+        }
+        if (sizeStr == 0) printf("Вершина %d изолированная\n", i+1);//комментарии для теста
         if (sizeStr == 1) printf("Вершина %d концевая\n", i+1);
         if (sizeStr >= n-1) printf("Вершина %d доминирующая\n", i+1);
     }
